2b258df9e978811f5e4ece498956dfa7e4e760df
BestBuyMetalsDev01 <jacob@bestbuymetals.com>
2025-07-11 11:10:30 -0400

    commit: Pole Barn Quote Form React App

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/https://github.com/BestBuyMetalsDev01/PoleBarnForm.git/', // IMPORTANT: Replace 'your-repo-name' with your actual GitHub repository name
+    base: '/', // IMPORTANT: Replace 'your-repo-name' with your actual GitHub repository name
 
 })
