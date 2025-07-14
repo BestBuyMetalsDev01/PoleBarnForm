b32421614c126f6c8ca1c8102b24ca48355df089
BestBuyMetalsDev01 <jacob@bestbuymetals.com>
2025-07-14 09:19:33 -0400

    Initial commit: Pole Barn Quote Form React App

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // <--- Add this line or change it if it's different
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
